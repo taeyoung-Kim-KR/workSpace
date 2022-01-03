91aac46fea28ba31c966981cfd52e35a02f45d89
taeyoung-Kim-KR <75604128+taeyoung-Kim-KR@users.noreply.github.com>
2022-01-03 22:06:21 +0900

    create new project

---
+++ OddEvenGame/OddEvenGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OddEvenGame
+//
+//  Created by 김태영 on 2022/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
